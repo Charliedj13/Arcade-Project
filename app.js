ca1d792319dcc8de4e5e0ec74aa61f04b1f9830d
Charlie Jenkins <charliedj13@gmail.com>
2023-02-12 11:19:20 -0700

    final save clear

--- app.js
+++ app.js
@@ -1,39 +1,81 @@
 // Variables from HTML
+let tableElement = document.getElementById("pro")
+let playerSelection = document.getElementById("player")
 
 // Variables from JS
 let gameStarted = false
 
 
-window.addEventListener("click", function () {
+window.addEventListener("DOMContentLoaded", function () {
  if (gameStarted == false){
     console.log("Is this working")
     buildGrid ()
+    playerTurn ()
     gameStarted = true
  }
 })
+let gameBoardTTT = [
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined]
+]
+
+function cellCallbackFunction(event) {
+    console.log(event)
+    console.log(event.target)
+    
+}
+
 
 const table = document.getElementsByTagName("table")[0]
 
 function buildGrid () {
-    for (let numOfRows = 0; numOfRows < 3; numOfRows++){
+    let numberTrackerRow = 0
+    for (let indexPosition = 0; indexPosition < gameBoardTTT.length; indexPosition++){
     let newRow = document.createElement("tr")
-    newRow.style.backgroundColor = "red"
-    newRow.style.border = "2px solid black"
+    // newRow.className = "cell"
+        newRow.style.backgroundColor = "red"
+        newRow.style.border = "2px solid black"
+        newRow.classList.add(indexPosition)
 
-    for (let numOfColumns = 0; numOfColumns < 3; numOfColumns++) {
-        let newColumn = document.createElement("td") 
+    for (let numOfColumns = 0; numOfColumns < gameBoardTTT.length; numOfColumns++) {
+        let newCell = document.createElement("td") 
+        newCell.classList.add("cell")
 
-        newColumn.style.backgroundColor = "red"
-        newColumn.style.border = "2px solid black"
-        
-        newRow.appendChild(newColumn)
+        // newRow.classList.add(indexPosition)
+        newCell.classList.add(numberTrackerRow)
+        numberTrackerRow++
+
+        newCell.addEventListener("click", cellCallbackFunction)
+
+        newRow.appendChild(newCell)
     }
+
     tableElement.appendChild(newRow)
 }
 }
 
-// const gameState = {
-//     players: ["x", "o"]
-    
-// }
+
+const gameState = {
+    players: ['x', 'o']
+}
+function playerTurn (){
+    let player = Math.ceil(Math.random () * 2)
+    if (player == 1){
+        gameState == "x"
+        
+        console.log("It is X turn")
+        
+    } else if (player == 2){
+        gameState == "o"
+
+        console.log("It is O turn")
+        
+    }
+}
+
+
+
+
+
 
